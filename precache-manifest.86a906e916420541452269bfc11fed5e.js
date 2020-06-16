10c0f12b438e321dc38eb9e21d56361c56ce36af
Mahir kemal Kara <mahirkemalkara@Mahir-MBP.home>
2020-06-16 23:40:58 +0300

    Updates

---
+++ precache-manifest.86a906e916420541452269bfc11fed5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ab00926adc22d10d8f53dfdd03fd33",
+    "url": "/mahirkemal/caglarkaraweb/index.html"
+  },
+  {
+    "revision": "2f739efb7d2f03ee7e54",
+    "url": "/mahirkemal/caglarkaraweb/static/css/main.8b5d00fa.chunk.css"
+  },
+  {
+    "revision": "9281b22b77d3bf09fa6c",
+    "url": "/mahirkemal/caglarkaraweb/static/js/2.304f183f.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/mahirkemal/caglarkaraweb/static/js/2.304f183f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f739efb7d2f03ee7e54",
+    "url": "/mahirkemal/caglarkaraweb/static/js/main.431670a5.chunk.js"
+  },
+  {
+    "revision": "1abb95d9503c3e02f592",
+    "url": "/mahirkemal/caglarkaraweb/static/js/runtime-main.492c90dc.js"
+  }
+]);
